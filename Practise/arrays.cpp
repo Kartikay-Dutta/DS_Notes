1bd76e0dc1d805c51bc61215a18ce5540cd6f982
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-20 11:50:05 +0530

    code

---
+++ Practise/arrays.cpp
@@ -0,0 +1,6 @@
+// reversing an array
+#include<iostream>
+using namespace std;
+int main(){
+    
+}
---
+++ Practise/class.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class Test{
+    public:
+    int x = 10;
+    private:
+    int y = 20;
+};
+int main(){
+    Test t;
+    cout<<t.x;
+    // cout<<t.y;
+
+}         
