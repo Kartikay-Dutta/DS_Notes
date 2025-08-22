b098a3e05d67690ae7d488df31f8aa84d0b6e662
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-22 18:10:38 +0530

    code

--- Practise/arrays.cpp
+++ Practise/arrays.cpp
@@ -1,6 +1,81 @@
-// reversing an array
+// Reversing an array
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+
+//     // Input
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     // Reverse in-place
+//     for(int i = 0; i < n/2; i++) {
+//         int temp = arr[i];
+//         arr[i] = arr[n-i-1];
+//         arr[n-i-1] = temp;
+//     }
+
+//     // Print reversed array
+//     for(int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+
+// Largest number
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     int arr[n];
+//     cin>>n;
+//     for(int i = 0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int max = 0;
+//     for(int i= 0;i<n;i++){
+//         if(arr[i]>max){
+//             max=arr[i];
+//         }
+//     }
+//     cout<<max;
+// }
+
+
+// Palindrome
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string s;
+//     cin>>s;
+//     int n = s.length();
+//     bool isPalin = true;
+//     for(int i = 0;i<n/2;i++){
+//         if(s[i]!=s[n-i-1]){
+//             isPalin = false;
+//             break;
+//         }
+//     }
+//     if(isPalin == true){
+//         cout<<"It is a palindrome.";
+//     }
+//     else{
+//         cout<<"It is not a Palindrome";
+//     }
+// }
+
+
+// Armstrong number
 #include<iostream>
 using namespace std;
 int main(){
+    int n;
     
+
 }
