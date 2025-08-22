7f0ca54e332d9b50690bf981054ceb7f4fa1c4e2
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-22 18:24:08 +0530

    code

--- Practise/arrays.cpp
+++ Practise/arrays.cpp
@@ -71,11 +71,41 @@
 // }
 
 
-// Armstrong number
-#include<iostream>
+// Frequency of a num in array
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    
 
+int main() {
+    int arr[100], freq[100], n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    cout << "Enter elements of array:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        freq[i] = -1;
+    }
+
+    for(int i = 0; i < n; i++) {
+        int count = 1;
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                freq[j] = 0;
+            }
+        }
+        if(freq[i] != 0) {
+            freq[i] = count;
+        }
+    }
+
+    cout << "Element\tFrequency\n";
+    for(int i = 0; i < n; i++) {
+        if(freq[i] != 0) {
+            cout << arr[i] << "\t" << freq[i] << endl;
+        }
+    }
+
+     return 0;
 }
